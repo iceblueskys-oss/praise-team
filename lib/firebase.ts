15ed7e42af1a22e8d44852721e510da3d0db0fdc
iceblueskys-oss <iceblueskys@gmail.com>
2026-08-26 22:05:11 +0900

    Update firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,6 +1,5 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDi9jZ5a3q01LUqCiR6d-4OELjtpAFTLQo",
@@ -11,7 +10,5 @@ const firebaseConfig = {
   appId: "1:921722710189:web:52e992a5ce8daf5c5f3e24"
 };
 
-// Next.js 환경에서 중복 초기화 방지
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 export const db = getFirestore(app);
-export const storage = getStorage(app);
