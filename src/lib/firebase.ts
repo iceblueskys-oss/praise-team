558dbc89d644f340ab6dafd02ef15a87a2dcb7db
iceblueskys-oss <iceblueskys@gmail.com>
2026-09-13 06:13:58 +0900

    Fix storageBucket URL in firebase configuration

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -7,7 +7,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyDi9jZ5a3q01LUqCiR6d-4OELjtpAFTLQo",
   authDomain: "praisesheet-66fe5.firebaseapp.com",
   projectId: "praisesheet-66fe5",
-  storageBucket: "praisesheet-66fe5.firebasestorage.app",
+  storageBucket: "praisesheet-66fe5.appspot.com",
   messagingSenderId: "921722710189",
   appId: "1:921722710189:web:52e992a5ce8daf5c5f3e24"
 };
